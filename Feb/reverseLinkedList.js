e0f50419a5d9576da67c108ce736de79449099ca
jparrker <102701917+jparrker@users.noreply.github.com>
2023-02-28 21:23:20 -0500

    fixed comments

--- Feb/reverseLinkedList.js
+++ Feb/reverseLinkedList.js
@@ -1,54 +1,51 @@
-**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
+// **
+//  * Definition for singly-linked list.
+//  * function ListNode(val, next) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.next = (next===undefined ? null : next)
+//  * }
+//  */
+// /**
+//  * @param {ListNode} head
+//  * @return {ListNode}
+//  */
 
 class LinkedList {
-  constructor () {
-    this.head = this.tail = null
+  constructor() {
+    this.head = this.tail = null;
   }
 
   append(value) {
-
     if (!this.tail) {
-      this.head = this.tail = new Node(value)
-    }
- 
-    else {
-      let oldTail = this.tail
-      this.tail = new Node(value)
-      oldTail.next = this.tail
+      this.head = this.tail = new Node(value);
+    } else {
+      let oldTail = this.tail;
+      this.tail = new Node(value);
+      oldTail.next = this.tail;
     }
   }
-  reverseList(){
-     let current = this.head
-     let prev = null
-     while (current) {
-        const temp = current.next
-        current.next = prev
-        prev = current
-        current = temp
+  reverseList() {
+    let current = this.head;
+    let prev = null;
+    while (current) {
+      const temp = current.next;
+      current.next = prev;
+      prev = current;
+      current = temp;
     }
-    return prev
+    return prev;
   }
 }
 
 class Node {
-  constructor (value, prev, next) {
-    this.value = value
-    this.next = next || null
-   }
+  constructor(value, prev, next) {
+    this.value = value;
+    this.next = next || null;
+  }
 }
 
-const list = new LinkedList()
+const list = new LinkedList();
 
-list.append(1)
-list.append(2)
-list.append(3)
+list.append(1);
+list.append(2);
+list.append(3);
